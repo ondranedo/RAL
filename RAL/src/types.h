6e9e9b5cfa78e3d19a5fbdc3028eb4f8be93e3ce
Karnatour <vojtasokala@gmail.com>
2023-02-27 22:04:52 +0100

    Vectors

--- RAL/src/types.h
+++ RAL/src/types.h
@@ -1,22 +1,34 @@
 #pragma once
 
-using i8_t = signed char;			//1B
-using u8_t = unsigned char;			//1B
+#include <iostream>
+#include <vector>
+#include <string>
 
-using i16_t = short;				//2B
-using u16_t = unsigned short;		//2B
+using i8_t = signed char;
+using u8_t = unsigned char;
+using i8vec_t = std::vector<char>;
+using u8vec_t = std::vector<unsigned char>;
 
-using i32_t = int;					//4B
-using u32_t = unsigned int;			//4B
+using i16_t = short;
+using u16_t = unsigned short;
+using i16vec_t = std::vector<short>;
+using u16vec_t = std::vector<unsigned short>;
 
-using i64_t = long long;			//8B
-using u64_t = unsigned long long;	//8B
+using i32_t = int;
+using u32_t = unsigned int;
+using i32vec_t = std::vector<int>;
+using u32vec_t = std::vector<unsigned int>;
 
-using f32_t = float;				//4B
+using i64_t = long long;
+using u64_t = unsigned long long;
+using i64vec_t = std::vector<long long>;
+using u64vec_t = std::vector<unsigned long long>;
 
-using f64_t = double;				//8B
+using f32_t = float;
+using f32vec_t = std::vector<float>;
 
-
+using f64_t = double;
+using f64vec_t = std::vector<double>;
 
 
 
