cf81acb5866451b7109c41e080a7caced6c98a76
ondranedo <nedojedly.o@gmail.com>
2023-02-28 17:14:59 +0100

    RAL_ASSERT_NEGATIVE_INDEX -< RAL_ASSERT_NEGATIVE

--- RAL/src/asserts.h
+++ RAL/src/asserts.h
@@ -9,14 +9,14 @@
 #include <cassert>
 
 #define RAL_ASSERT_UNREACHABLE() assert(0)
-#define RAL_ASSERT_NEGATIVE_INDEX(point) assert(point >= 0)
+#define RAL_ASSERT_NEGATIVE(point) assert(point >= 0)
 #define RAL_ASSERT(expr) assert(expr != NULL)
 
 #endif
 #ifdef RAL_RELEASE
 
 #define RAL_ASSERT_UNREACHABLE()
-#define RAL_ASSERT_NEGATIVE_INDEX()
+#define RAL_ASSERT_NEGATIVE()
 #define RAL_ASSERT(expr)
 
 #endif
