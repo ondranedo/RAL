935915dd71cbb48243a3c7a34d395e223d4e4b5a
Adam <116303413+1nome@users.noreply.github.com>
2023-02-28 19:43:57 +0100

    Added a simple function for printing info

--- RAL/src/asserts.cpp
+++ RAL/src/asserts.cpp
@@ -1,3 +1,11 @@
 /*
-	needs a logger implementation
+	function will print the line number, file and a custom message
+	can change const char if necessary
 */
+
+#include <stdio.h>
+
+void info(const char* message){
+	
+	printf("File: %s Line: %s | %s", __FILE__, __LINE__, message);
+}
