84c3ee199998fee97f34ca51d444f0ee475ecd89
Sutiama <matyas.noska@icloud.com>
2023-02-28 17:21:26 +0100

    Log - #define update

--- RAL/src/logger.h
+++ RAL/src/logger.h
@@ -107,31 +107,23 @@ namespace RAL
 #ifdef RAL_DEBUG
 #define RAL_LOG_TRACE(message,...) RAL::Logger::Trace(message,__VA_ARGS__);
 #define RAL_LOG_DEBUG(message,...) RAL::Logger::Debug(message,__VA_ARGS__);
-#define RAL_LOG_INFO(message,...) RAL::Logger::Info(message,__VA_ARGS__);
-#define RAL_LOG_WARN(message,...) RAL::Logger::Warning(message,__VA_ARGS__);
-#define RAL_LOG_ERROR(message,...) RAL::Logger::Error(message,__VA_ARGS__);
-#define RAL_LOG_CRIT(message,...) RAL::Logger::Critical(message,__VA_ARGS__);
 #define RAL_LOG_PRIORITY_TRACE() RAL::Logger::setPriority(RAL::Logger::Priority::Trace);
 #define RAL_LOG_PRIORITY_DEBUG() RAL::Logger::setPriority(RAL::Logger::Priority::Debug);
-#define RAL_LOG_PRIORITY_INFO() RAL::Logger::setPriority(RAL::Logger::Priority::Info);
-#define RAL_LOG_PRIORITY_WARN() RAL::Logger::setPriority(RAL::Logger::Priority::Warning);
-#define RAL_LOG_PRIORITY_ERROR() RAL::Logger::setPriority(RAL::Logger::Priority::Error);
-#define RAL_LOG_PRIORITY_CRITICAL() RAL::Logger::setPriority(RAL::Logger::Priority::Critical);
 #endif
 
 #ifdef RAL_RELEASE
 #define RAL_LOG_TRACE(message,...)
 #define RAL_LOG_DEBUG(message,...)
+#define RAL_LOG_PRIORITY_TRACE()
+#define RAL_LOG_PRIORITY_DEBUG()
+#endif
+
 #define RAL_LOG_INFO(message,...) RAL::Logger::Info(message,__VA_ARGS__);
 #define RAL_LOG_WARN(message,...) RAL::Logger::Warning(message,__VA_ARGS__);
 #define RAL_LOG_ERROR(message,...) RAL::Logger::Error(message,__VA_ARGS__);
 #define RAL_LOG_CRIT(message,...) RAL::Logger::Critical(message,__VA_ARGS__);
-#define RAL_LOG_SETPRIORITY(priority) RAL::Logger::setPriority(priority);
-#define RAL_LOG_PRIORITY_TRACE()
-#define RAL_LOG_PRIORITY_DEBUG()
 #define RAL_LOG_PRIORITY_INFO() RAL::Logger::setPriority(RAL::Logger::Priority::Info);
 #define RAL_LOG_PRIORITY_WARN() RAL::Logger::setPriority(RAL::Logger::Priority::Warning);
 #define RAL_LOG_PRIORITY_ERROR() RAL::Logger::setPriority(RAL::Logger::Priority::Error);
 #define RAL_LOG_PRIORITY_CRITICAL() RAL::Logger::setPriority(RAL::Logger::Priority::Critical);
-#endif
 
