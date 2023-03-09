bd7eb39a3cec03c81823f33ed34eddb7c5d2dfe6
1nome <stashfoos@gmail.com>
2023-03-09 18:44:44 +0100

    created an untested MemoryFactory

--- RAL/src/platformLayer/memory/memory.h
+++ RAL/src/platformLayer/memory/memory.h
@@ -1,47 +1,29 @@
 #pragma once
+
 #include "../../pch.h"
-
-/*TODO: pointer validation
-*		runtime checks
-*		add more guard bytes
-*		add constants to make adding more guard bytes easier
-*		add asserts for out of bounds access
-*/
-
-#include <cstdlib>
-
-#ifdef RAL_DEBUG
-
-#define GUARD_ONE 0xB0
-#endif
-
 namespace RAL {
+    class Memory {
 
-	class Memory {
-
-	protected:
-		i64_t nOfBytes;
+    protected:
+        i64_t nOfBytes;
 #ifdef RAL_DEBUG
-		i64_t nOfGuards;
+        i64_t nOfGuards;
 #endif
-		i64_t nOfBlocks;
+        i64_t nOfBlocks;
 
-	public:
-		Memory();
-		~Memory();
+    public:
+        Memory();
 
-		virtual void* allocate(size_t bytes) = 0;
-		virtual void release(void* block) = 0;
-		virtual void* reallocate(void* block, size_t newSize) = 0;
-		i64_t allocated();
-		i64_t blocks();
-	};
+        virtual ~Memory();
 
-	class WinMemory: public Memory {
+        virtual void *allocate(size_t bytes) = 0;
 
-	public:
-		void* allocate(size_t bytes);
-		void release(void* block);
-		void* reallocate(void* block, size_t newSize);
-	};
+        virtual void release(void *block) = 0;
+
+        virtual void *reallocate(void *block, size_t newSize) = 0;
+
+        i64_t allocated();
+
+        i64_t blocks();
+    };
 }
