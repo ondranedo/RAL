5221b263a46ff46138373b393f83bca3d146cac1
1nome <stashfoos@gmail.com>
2023-03-09 18:47:43 +0100

    created an untested MemoryFactory

---
+++ RAL/src/platformLayer/memory/wMemory.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "memory.h"
+
+/*      pointer validation
+*		runtime checks
+*		add more guard bytes
+*		add constants to make adding more guard bytes easier
+*		add asserts for out of bounds access
+*/
+
+#ifdef RAL_WINDOWS
+
+#include <cstdlib>
+
+#ifdef RAL_DEBUG
+
+#define GUARD_ONE 0xB0
+#endif
+
+namespace RAL {
+
+	class WinMemory: public Memory {
+
+	public:
+        WinMemory();
+        ~WinMemory() override;
+
+		void* allocate(size_t bytes) override;
+		void release(void* block) override;
+		void* reallocate(void* block, size_t newSize) override;
+	};
+}
+#endif
