942d842ebc4c9eb03c0267526e0742861d5537e1
1nome <stashfoos@gmail.com>
2023-03-12 13:43:09 +0100

    added method declarations

--- RAL/src/core/factoryManager.h
+++ RAL/src/core/factoryManager.h
@@ -4,6 +4,21 @@ namespace RAL{
 
     class FactoryMgr{
 
+    public:
+        void addFactory();
 
+        void addComponent();
+        void createComponent();
+        void removeComponent();
+
+        void create();
+        void init();
+        void release();
+
+        void get();
+
+    private:
+        BaseFactory* factories;
+        BaseComponent* components;
     };
 }
