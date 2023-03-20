169784cc75fe135c5f4679061dfa1ab9ebbcca35
1nome <stashfoos@gmail.com>
2023-03-20 17:09:55 +0100

    moved templated methods to .h

--- RAL/src/core/factoryManager.cpp
+++ RAL/src/core/factoryManager.cpp
@@ -4,51 +4,6 @@
 
 namespace RAL {
 
-    template<typename factory>
-    void FactoryComponentMgr::addFactory() {
-
-        String temp2 = stringize(factory);
-        struct Factory temp;
-        u64_t i;
-
-        RAL_FACTORY_SCANTHRU{
-            if(m_factories[i].m_productName == temp2){
-                RAL_ASSERT_MSG("Factory %s already created!", temp2.c_str());
-            }
-        };
-
-        temp.m_productName = temp2;
-        temp.m_hadDefaultCreated = false;
-        temp.m_factory = mainMemory.alloc<factory>();
-
-        m_factories.push_back(temp);
-    }
-
-    template<typename factory> void FactoryComponentMgr::createComponent(const String& name){
-
-        u64_t i;
-        RAL_COMPONENT_SCANTHRU
-            RAL_COMPONENT_ISNAME
-                RAL_ASSERT_MSG("Component %s already created!", name.c_str());
-
-        String temp2 = stringize(factory);
-
-        if(temp2 == name)
-            RAL_ASSERT_MSG("Cant create a non-default component with a default name (%s)", name.c_str());
-
-        RAL_FACTORY_SCANTHRU{
-            if(m_factories[i].m_productName == temp2){
-                struct Component temp;
-                temp.m_wasInitialized = false;
-                temp.m_name = name;
-                temp.m_component = m_factories[i].m_factory->create();
-                m_components.push_back(temp);
-                return;
-            }
-        };
-        RAL_ASSERT_MSG("Factory %s not found or created!", temp2.c_str());
-    }
-
     BaseComponent* FactoryComponentMgr::operator[](const String &name) {
 
         u64_t i;
