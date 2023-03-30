0eff7c307353ef13bccd0eb3d4de7ea2f9151e2c
ondranedo <nedojedly.o@gmail.com>
2023-03-30 06:50:34 +0200

    clean

--- RAL/src/events/Event.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2023 Ondřej Nedojedlý (https://www.github.com/ondranedo).
-// This file is part of `RAL_PROJECT` project which is released under MIT licence.
-// See file LICENSE for full license details.
-
-#include "Event.h"
-
-namespace RAL {
-} // RAL
