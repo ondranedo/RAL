23a16c9e4b0fc9e20643233dd5d7bfeb288e71f4
Vojtěch Šokala <vojtasokala@gmail.com>
2023-04-03 20:30:17 +0200

    RenderingContext

---
+++ RAL/renderer/renderingAPI/RenderingContext.h
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////////
+// Copyright (C) 2023                                  //
+// Ondřej Nedojedlý (https://www.github.com/ondranedo),//
+// Matyáš Noska (https://www.github.com/sutiama),      //
+// Vojtěch Šokala (https://github.com/Karnatour),      //
+// Adam Wiszczor (https://github.com/1nome).           //
+//                                                     //
+// This file is part of `RAL_PROJECT` project          //
+// which is released under `GNU General Public         //
+// License v3.0` license.                              //
+// See file `LICENSE` for full license details.        //
+/////////////////////////////////////////////////////////
+namespace RAL
+{
+    class RenderingContext
+    {
+    public:
+    };
+};
