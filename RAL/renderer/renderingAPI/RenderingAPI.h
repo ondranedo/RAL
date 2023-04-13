a8510dfcf9301031b40ecbe3b67cf182dc516726
Ondřej Nedojedlý <83519062+ondranedo@users.noreply.github.com>
2023-04-13 15:34:44 +0200

    commenting

--- RAL/renderer/renderingAPI/RenderingAPI.h
+++ RAL/renderer/renderingAPI/RenderingAPI.h
@@ -29,23 +29,51 @@ namespace RAL
         RenderingAPI();
         virtual ~RenderingAPI();
 
+        // Clears the screen with the clear colour
+        // this function is called before each frame is drawn
         virtual void clear() = 0;
+
+        // Can be used to set the clear colour, each value is in range 0-255.
+        // Each frame will be cleared to this colour.
         void clearColour(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+        // Can be used to set the clear colour, each value is in range 0-255.
+        // Each frame will be cleared to this colour. Alpha value will be set to 0xFF.
         void clearColour(uint8_t r, uint8_t g, uint8_t b);
-        // can be used to set the clear colour to a hex value
+
+        // Can be used to set the clear colour to a hex value
         // you can use alpha value by using 0xRRGGBBAA, where AA is the alpha value
         // if you don't want to use alpha value, use 0xRRGGBB, where AA will be set to 0xFF
         void clearColour(uint32_t hex);
 
+        // Crucial functions that must be called
+        // before calling init, the window must be set using setWindow,
+        // otherwise the engine will crash.
         virtual void init() = 0;
+
+        // Release function is called when the engine is shutting down
+        // it should release all resources that were allocated by the rendering API
         virtual void release() = 0;
 
+        // draw function is called every frame, it should draw the scene to the screen
+        // one draw call can draw multiple objects, but it can use only one shader.
         virtual void draw() = 0;
 
+        // Bind functions are used to bind the buffers to the rendering API.
+        // In other words, it sends the data from the buffers to the GPU.
         virtual void bind(const IndexBuffer& indexBuffer) = 0;
+
+        // Bind functions are used to bind the buffers to the rendering API.
+        // In other words, it sends the data from the buffers to the GPU.
         virtual void bind(const VertexBuffer& vertexBuffer) = 0;
 
+        // SetWindow function is used to set the window that will be drawn to.
+        // This function must be called before init is called. When we change the window,
+        // we must call clearWindow and then setWindow again.
         void setWindow(Window* window);
+
+        // This function is called when the window is changed. It should release all resources
+        // and stop drawing to the old window.
         void clearWindow();
 
     private:
