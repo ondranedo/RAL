549ac1165fcc964e6fdd106a40bd71baa20ba357
1nome <stashfoos@gmail.com>
2023-04-15 20:59:39 +0200

    Texture files, saw png encoding, and now I want to stop writing texture loading

---
+++ RAL/renderer/texture/Texture.cpp
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////
+// Copyright (C) 2023                                  //
+// Ondřej Nedojedlý (https://www.github.com/ondranedo),//
+// Matyáš Noska (https://www.github.com/sutiama),      //
+// Vojtěch Šokala (https://github.com/Karnatour),      //
+// Adam Wiszczor (https://github.com/1nome).           //
+//                                                     //
+// This file is part of `RAL_PROJECT` project          //
+// which is released under `GNU General Public         //
+// License v3.0` license.                              //
+// See file `LICENSE` for full license details.        //
+/////////////////////////////////////////////////////////
+
+#include "Texture.h"
+
+namespace RAL{
+
+    uint16_t Texture::getWidth() {
+        return m_image.data()->size();
+    }
+
+    uint16_t Texture::getHeight() {
+        return m_image.size();
+    }
+
+    size_t Texture::getSize() {
+        return getWidth() * getHeight() * sizeof(Pixel);
+    }
+}
---
+++ RAL/renderer/texture/Texture.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////
+// Copyright (C) 2023                                  //
+// Ondřej Nedojedlý (https://www.github.com/ondranedo),//
+// Matyáš Noska (https://www.github.com/sutiama),      //
+// Vojtěch Šokala (https://github.com/Karnatour),      //
+// Adam Wiszczor (https://github.com/1nome).           //
+//                                                     //
+// This file is part of `RAL_PROJECT` project          //
+// which is released under `GNU General Public         //
+// License v3.0` license.                              //
+// See file `LICENSE` for full license details.        //
+/////////////////////////////////////////////////////////
+
+#ifndef RAL_PROJECT_TEXTURE_H
+#define RAL_PROJECT_TEXTURE_H
+
+#include <vector>
+#include <string>
+
+namespace RAL{
+    class Texture{
+    public:
+        struct Pixel{
+            uint8_t m_red;
+            uint8_t m_green;
+            uint8_t m_blue;
+            uint8_t m_alpha;
+        };
+
+        Texture();
+        ~Texture();
+
+        uint16_t getWidth();
+        uint16_t getHeight();
+        size_t getSize();
+
+    private:
+        std::vector<std::vector<Pixel>>* m_image;
+    };
+}
+
+#endif //RAL_PROJECT_TEXTURE_H
