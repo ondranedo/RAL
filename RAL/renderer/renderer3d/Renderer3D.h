f227d0918e1b03271a4c27f6691b80ee8d912d6c
Ondřej Nedojedlý <83519062+ondranedo@users.noreply.github.com>
2023-04-25 13:21:10 +0200

    Renderer3D class

---
+++ RAL/renderer/renderer3d/Renderer3D.h
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////
+// Copyright (C) 2023                                  //
+// Ondřej Nedojedlý (https://www.github.com/ondranedo),//
+// Matyáš Noska (https://www.github.com/sutiama),      //
+// Vojtěch Šokala (https://github.com/Karnatour),      //
+// Adam Wiszczor (https://github.com/1nome).           //
+//                                                     //
+// This file is part of `RAL_PROJECT` project          //
+// which is released under `GNU General Public         //
+// License v3.0` license.                              //
+// See file `LICENSE` for full license details.        //
+/////////////////////////////////////////////////////////
+#ifndef RAL_PROJECT_RENDERER3D_H
+#define RAL_PROJECT_RENDERER3D_H
+
+namespace RAL {
+
+    class Renderer3D {
+
+    };
+
+} // RAL
+
+#endif //RAL_PROJECT_RENDERER3D_H
