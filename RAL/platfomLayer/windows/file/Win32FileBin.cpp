92052574f1412999c99316dc23b6cd812b801ced
1nome <stashfoos@gmail.com>
2023-04-25 13:13:22 +0200

    more method prototyping, getModeInFormat()

--- RAL/platfomLayer/windows/file/Win32FileBin.cpp
+++ RAL/platfomLayer/windows/file/Win32FileBin.cpp
@@ -12,3 +12,26 @@
 /////////////////////////////////////////////////////////
 
 #include "Win32FileBin.h"
+#include <core/utility/Asserts.h>
+
+namespace RAL::Win32{
+    const char *Win32FileBin::getModeInFormat(File::Mode mode) {
+        switch (mode) {
+            case File::Mode::Read:      return "rb";
+            case File::Mode::Write:     return "wb";
+            case File::Mode::ReadWrite: return "wb+";
+            case File::Mode::Append:    return "ab";
+            default:
+                RAL_ASSERTR(false, "rb", "unknown file mode!, defaulting to read");
+        }
+    }
+
+    void Win32FileBin::reset() const {
+        RAL_ASSERTRV(m_file != nullptr, "file is not open!");
+        std::rewind(m_file);
+    }
+
+    Win32FileBin::~Win32FileBin() = default;
+
+    Win32FileBin::Win32FileBin() = default;
+}
--- RAL/platfomLayer/windows/file/Win32FileBin.h
+++ RAL/platfomLayer/windows/file/Win32FileBin.h
@@ -14,4 +14,26 @@
 #ifndef RAL_PROJECT_WIN32FILEBIN_H
 #define RAL_PROJECT_WIN32FILEBIN_H
 
+#include <platfomLayer/file/FileBin.h>
+#include <platfomLayer/windows/file/Win32File.h>
+
+namespace RAL::Win32{
+    class Win32FileBin : public virtual FileBin, public virtual Win32File{
+    public:
+        Win32FileBin();
+        ~Win32FileBin() override;
+
+        void readInto(void *dest, size_t elementSize, size_t elementCount) override;
+        void readInto(void *dest, size_t elementSize) override;
+
+        void writeFrom(const void *src, size_t elementSize, size_t elementCount) override;
+        void writeFrom(const void *src, size_t elementSize) override;
+
+        void reset() const override;
+
+    private:
+        const char* getModeInFormat(Mode mode) override;
+    };
+}
+
 #endif //RAL_PROJECT_WIN32FILEBIN_H
